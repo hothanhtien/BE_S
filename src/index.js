693005590c70ebd8fe1d82519b8991775f5118ce
Bunkid <85295527+linhprovip2002@users.noreply.github.com>
2024-05-20 07:27:48 +0000

    fix/APIs for user  data json

--- src/index.js
+++ src/index.js
@@ -6,20 +6,8 @@ const port = process.env.PORT || 3000
 app.use(express.urlencoded());
 app.use(express.json())
 
-const checkAuthentication = (req, res, next) => {
-    const accessToken = req.headers.authorization;
-    if (accessToken) {
-        console.log('middleware to authen')
-        req.user = accessToken;
-        next()
-    }
-    else {
-        res.send('user chưa login')
-    }
-}
   
 app.get('/users'
-    ,checkAuthentication
     ,(req, res) => {
     req.user // biết ai đang đăng nhập 
     fs.readFile('data.json', 'utf8', (err, data) => {
@@ -33,43 +21,57 @@ app.get('/users'
   
   
   
-app.post('/create/:id', (req, res) => {
-console.log('3. Tạo người dùng mới')
-console.log('Các trường và value muốn tạo: ', req.body)
+// app.post('/create/:id', (req, res) => {
+// console.log('3. Tạo người dùng mới')
+// console.log('Các trường và value muốn tạo: ', req.body)
 
-const newUser = { //req.body luôn
-    id: parseInt(req.params.id),
-    name: req.body.name,
-    Age: parseInt(req.body.Age)
-}
-fs.readFile('data.json', 'utf8', (err, data) => {
-    if (err) {
-    res.send('lỗi đọc file');
-    return;
-    }
-    let dataUsers = JSON.parse(data)
-    console.log(dataUsers)
-    console.log(newUser)
-    const newData = [...dataUsers, newUser]
-    // console.log('hi',newData)
-    fs.writeFile('data.json', JSON.stringify(newData), (err) => {
-    if (err) {
-        res.send('lỗi khi viết file', err)
-        return
-    }
-    res.send('data đã được lưu')
+// const newUser = { //req.body luôn
+//     id: parseInt(req.params.id),
+//     name: req.body.name,
+//     Age: parseInt(req.body.Age)
+// }
+// fs.readFile('data.json', 'utf8', (err, data) => {
+//     if (err) {
+//     res.send('lỗi đọc file');
+//     return;
+//     }
+//     let dataUsers = JSON.parse(data)
+//     console.log(dataUsers)
+//     console.log(newUser)
+//     const newData = [...dataUsers, newUser]
+//     // console.log('hi',newData)
+//     fs.writeFile('data.json', JSON.stringify(newData), (err) => {
+//     if (err) {
+//         res.send('lỗi khi viết file', err)
+//         return
+//     }
+//     res.send('data đã được lưu')
+//     })
+// })
+
+// })
+
+app.post('/create', (req, res) => {
+    const { name, Age } = req.body
+    const data = JSON.stringify({ id: Date.now(),Age: Age, name: name })
+    fs.writeFile('data.json', data, (err) => {
+        if (err) {
+            res.send('Error writing file', err)
+            return
+        }
+        res.send('Data saved')
     })
-})
-
-})
+}
+)
 
 app.put('/update/:id', (req, res) => {
 console.log('4. Update dữ liệu')
 console.log('Id name Age bạn muốn update ', req.params.id)
-const name1 = req.body.name;
-const id1 = parseInt(req.params.id);
-const age1 = parseInt(req.body.Age)
-console.log('log ne', {id1, name1, age1})
+// const name1 = req.body.name;
+// const id1 = parseInt(req.params.id);
+// const age1 = parseInt(req.body.Age)
+const { name, Age} = req.body;
+// console.log('log ne', {id1, name1, age1})
 // const userUpdate = users.find(user => user.id === parseInt(req.params.id))
 // userUpdate.name = req.body.name;
 // userUpdate.Age = parseInt(req.body.Age);
@@ -82,9 +84,9 @@ fs.readFile('data.json', 'utf8', (err, data) => {
     const newData = JSON.parse(data).map((item) => {
         if (item.id == req.params.id)
         return {
-            id: id1, 
-            name: name1,
-            age: age1
+            id: parseInt(req.params.id), 
+            name: name,
+            age: Age
         }
         return item
     })
