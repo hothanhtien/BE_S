c7ae8e1211590719acb19ae2fd8746b3e0268f31
Ho Thanh Tien <hothanhtienqb@gmail.com>
2024-06-20 17:28:24 +0700

    Delete redundant comments

--- src/Controller/userControler.js
+++ src/Controller/userControler.js
@@ -13,15 +13,6 @@ class userControler {
             res.status(500).send('Server Error');
         }
     };
-    // display(req, res, next) {
-    //     fs.readFile('data.json', 'utf8', (err, data) => {
-    //         if (err) {
-    //             res.send('lỗi đọc file')
-    //             return
-    //         }
-    //         res.send(data)
-    //     })
-    // }
     detial = async (req, res, next) => {
         try {
             const [usersDetial] = await pool.query('SELECT * FROM userdetial WHERE id = ?', req.params.id);
@@ -33,17 +24,6 @@ class userControler {
             console.error(err); 
             res.status(500).send('Server Error');
         }
-        // console.log('2. Get từng user cụ thể khi truyền params')
-        // console.log('ID muốn lấy ra thông qua params nè' ,req.params.id)
-        // fs.readFile('data.json', 'utf8', (err, data) => {
-        //     if (err) {
-        //     res.send('Lỗi đọc dữ liệu')
-        //     return
-        //     }
-        //     const user = JSON.parse(data).filter(element => element.id == req.params.id);
-        //     console.log(user)
-        //     res.send(user)
-        // })
     }
     create = async(req, res, next) => {
         try {
@@ -56,29 +36,6 @@ class userControler {
             console.error(err);
             res.status(500).send('Server Error');
         }
-        // console.log('3. Tạo người dùng mới')
-        // console.log('Các trường và value muốn tạo: ', req.body)
-    
-        // const newUser = { //req.body luôn
-        //     id: parseInt(Date.now()),
-        //     name: req.body.name,
-        //     Age: parseInt(req.body.Age)
-        // }
-        // fs.readFile('data.json', 'utf8', (err, data) => {
-        //     if (err) {
-        //         res.send('lỗi đọc file');
-        //         return;
-        //     }
-        //     let dataUsers = JSON.parse(data)
-        //     const newData = [...dataUsers, newUser]
-        //     fs.writeFile('data.json', JSON.stringify(newData), (err) => {
-        //     if (err) {
-        //         res.send('lỗi khi viết file', err)
-        //         return
-        //     }
-        //     res.send(newData)
-        //     })
-        // })
     }
     update = async(req, res, next) => {
         try {
@@ -91,31 +48,6 @@ class userControler {
             console.error(err);
             res.status(500).send('Server Error');
         }
-        // console.log('4. Update dữ liệu')
-        // console.log('Id name Age bạn muốn update ', req.params.id)
-        // const { name, Age} = req.body;
-        // fs.readFile('data.json', 'utf8', (err, data) => {
-        //     if (err) {
-        //         res.send('Lỗi đọc file')
-        //         return
-        //     }
-        //     const newData = JSON.parse(data).map((item) => {
-        //         if (item.id == req.params.id)
-        //         return {
-        //             id: parseInt(req.params.id), 
-        //             name: name,
-        //             age: Age
-        //         }
-        //         return item
-        //     })
-        //     fs.writeFile('data.json', JSON.stringify(newData), (err) => {
-        //         if (err) {
-        //         res.send('Lỗi ghi dữ liệu')
-        //         return
-        //         }
-        //         res.send(newData)
-        //     })
-        // })
     }
     delete= async(req, res, next) =>{
         try {
@@ -126,27 +58,6 @@ class userControler {
             console.error(err);
             res.status(500).send('Server Error');
         }
-        // console.log('5. Xóa dữ liệu qua id')
-        // console.log('Id người nhập muốn xóa' ,req.params.id)
-        // fs.readFile('data.json', 'utf8', (err, data) => {
-        //     if (err) {
-        //     res.send('Lỗi đọc dữ liệu')
-        //     return
-        //     }
-        //     const dataParse = JSON.parse(data);
-        //     const userIndex = dataParse.findIndex(element => element.id == req.params.id);
-        //     console.log(userIndex)
-        //     dataParse.splice(userIndex, 1);
-        //     console.log(dataParse)
-        //     // res.send('ok')
-        //     fs.writeFile('data.json', JSON.stringify(dataParse), (err) => {
-        //     if (err) {
-        //         res.send('Lỗi ghi dữ liệu')
-        //         return
-        //     }
-        //     res.send(dataParse)
-        //     })
-        // })
     }
 }
 
